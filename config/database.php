61554791ad33cc05cb853592fbea9760b17c4737
Mohamed barhoumi <m.barhoumi@bluepenlabs.com>
2019-01-31 14:59:09 +0100

    upated config

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'dt_olympiade_sports_web'),
             'username' => env('DB_USERNAME', 'devteam'),
-            'password' => env('DB_PASSWORD', 'Devteam1$$'),
+            'password' => env('DB_PASSWORD', 'devteam$$'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
